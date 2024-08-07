8a0e90b5a464462caef28e0fb5ed21fa5dbf54e1
KAMATAKI Yuya <k212302@cc.k.tsukuba-tech.ac.jp>
2024-08-07 16:19:09 +0900

    Implement PresetVoice

--- aqtalk.go
+++ aqtalk.go
@@ -35,3 +35,77 @@ const (
 	VoiceBaseF2E VoiceBase = iota
 	VoiceBaseM1E VoiceBase = iota
 )
+
+// プリセットの声質
+var (
+	// 女性1
+	PresetVoiceF1 PARAM = PARAM{
+		VoiceBaseF1E,
+		100,
+		100,
+		100,
+		100,
+		100,
+		100,
+	}
+	// 女性2
+	PresetVoiceF2 PARAM = PARAM{
+		VoiceBaseF2E,
+		100,
+		100,
+		77,
+		150,
+		100,
+		100,
+	}
+	// 女性3
+	PresetVoiceF3 PARAM = PARAM{
+		VoiceBaseF1E,
+		80,
+		100,
+		100,
+		100,
+		61,
+		148,
+	}
+	// 男性1
+	PresetVoiceM1 PARAM = PARAM{
+		VoiceBaseF1E,
+		80,
+		100,
+		100,
+		100,
+		61,
+		148,
+	}
+	// 男性2
+	PresetVoiceM2 PARAM = PARAM{
+		VoiceBaseM1E,
+		100,
+		100,
+		30,
+		100,
+		100,
+		100,
+	}
+	// ロボット1
+	PresetVoiceR1 PARAM = PARAM{
+		VoiceBaseM1E,
+		105,
+		100,
+		45,
+		130,
+		120,
+		100,
+	}
+	// ロボット2
+	PresetVoiceR2 PARAM = PARAM{
+		VoiceBaseF2E,
+		70,
+		100,
+		50,
+		50,
+		50,
+		180,
+	}
+)
